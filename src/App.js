18a0d36db4951c242076294b40f7597194ba8cc9
arulselviusp <arulselviusp@gmail.com>
2022-02-06 15:34:10 +0530

    Todo

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Forms from "./components/Forms";
+import TodoList from "./components/Todolist";
+import todo from "../src/todo.svg";
+
+//* get localstorage items
+
+function App() {
+  //For input texts
+  const [inputText, setInputText] = useState("");
+
+  //For todos to render on UI
+  const [todos, setTodos] = useState([]);
+
+  //For filtering todos on UI
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  //useEffect for filterHandler
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+
+  //filterHandler
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">TODO-APP</header>
+      <figure className="App-picture">
+        <img src={todo} alt="todo" />
+      </figure>
+      <Forms
+        inputText={inputText}
+        setInputText={setInputText}
+        todos={todos}
+        setTodos={setTodos}
+        setStatus={setStatus}
+      />
+      <TodoList
+        filteredTodos={filteredTodos}
+        todos={todos}
+        setTodos={setTodos}
+      />
+    </div>
+  );
+}
+
+export default App;
